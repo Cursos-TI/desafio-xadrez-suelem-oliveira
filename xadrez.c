3cc2b84704448db5d144d1cce9f5a3a6ef5fcb86
suelem-oliveira <suelemoliveira934@gmail.com>
2025-08-22 15:47:19 -0300

    desafio de xadrez - Novato

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,41 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    const int BISPO_CASAS = 5;
+    const int TORRE_CASAS = 5;
+    const int RAINHA_CASAS = 8;
+    
+    int i; // Variável de controle para os loops
+    
+    printf("=== DESAFIO MATECHECK ===\n\n");
+    
+    // Implementação de Movimentação do Bispo
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("BISPO - Diagonal:\n");
+    for(i = 1; i <= BISPO_CASAS; i++) {
+        printf("Cima\n");
+        printf("Direita\n");
+    }
+    printf("\n");
+    
+    // Implementação de Movimentação da Torre
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("TORRE - Direita:\n");
+    i = 1;
+    while(i <= TORRE_CASAS) {
+        printf("Direita\n");
+        i++;
+    }
+    printf("\n");
+    
+    // Implementação de Movimentação da Rainha
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("RAINHA - Esquerda:\n");
+    i = 1;
+    do {
+        printf("Esquerda\n");
+        i++;
+    } while(i <= RAINHA_CASAS);
+    
     return 0;
 }
